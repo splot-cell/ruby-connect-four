6d375daadb803d6542508ef50755da8369d0e071
Olly Seber <olly_s@hotmail.co.uk>
2022-11-08 12:13:30 -0800

    Rewrite Board to access @grid[row][col]

--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class Board
-  def initialize(grid = Array.new(7, Array.new(6)))
+  def initialize(grid = Array.new(6, Array.new(7)))
     @grid = grid
   end
 
   def insert(item, col)
-    @grid[col][empty_row(col)] = item
+    @grid[empty_row(col)][col] = item
   end
 
   def empty_row(col)
-    @grid[col].find_index(nil)
+    @grid.length - @grid.transpose[col].reverse.find_index(nil) - 1
   end
 end
