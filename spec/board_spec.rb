32d64fb0c355a2ebf2241d8e7e78b69029f57ffa
Olly Seber <olly_s@hotmail.co.uk>
2022-11-08 12:18:47 -0800

    Reorder tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,34 +5,6 @@ require_relative "../lib/board"
 describe Board do
   subject(:board_init) { described_class.new }
 
-  describe "#insert" do
-    context "when a column is empty" do
-      it "adds an item to the lowest row on the column" do
-        item = "X"
-        col = 4
-        expect { board_init.insert(item, col) }.to change { board_init.instance_variable_get(:@grid)[5][col] }.from(nil).to(item)
-      end
-    end
-
-    context "when a column is not empty" do
-      let(:grid_non_empty) do
-        [[nil, nil, nil, nil, nil, nil, nil],
-         [nil, nil, nil, nil, nil, nil, nil],
-         [nil, nil, nil, nil, nil, nil, nil],
-         [nil, nil, nil, nil, nil, nil, nil],
-         [nil, nil, nil, nil, "B", nil, nil],
-         [nil, nil, nil, nil, "A", nil, nil]]
-      end
-      subject(:board_not_empty) { described_class.new(grid_non_empty) }
-
-      it "adds item to the lowest empty row in the columm" do
-        item = "C"
-        col = 4
-        expect { board_not_empty.insert(item, col) }.to change { board_not_empty.instance_variable_get(:@grid)[3][col] }.from(nil).to(item)
-      end
-    end
-  end
-
   describe "#empty_row" do
     context "when the column is empty" do
       it "returns index of last row" do
@@ -67,11 +39,11 @@ describe Board do
     context "when the column has more than one item taken" do
       let(:grid_half_full) do
         [[nil, nil, nil, nil, nil, nil, nil],
-         [nil, nil, nil, "D", nil, nil, nil],
-         [nil, nil, "S", "I", nil, "D", nil],
-         [nil, "G", "W", "V", "G", "N", nil],
-         ["T", "A", "O", "A", "G", "I", "O"],
-         ["A", "B", "C", "D", "E", "F", "G"]]
+        [nil, nil, nil, "D", nil, nil, nil],
+        [nil, nil, "S", "I", nil, "D", nil],
+        [nil, "G", "W", "V", "G", "N", nil],
+        ["T", "A", "O", "A", "G", "I", "O"],
+        ["A", "B", "C", "D", "E", "F", "G"]]
       end
       subject(:board_half_full) { described_class.new(grid_half_full) }
 
@@ -85,6 +57,34 @@ describe Board do
         it "returns index of sixth-last row" do
           expect(board_half_full.empty_row(3)).to eq(0)
         end
+      end
+    end
+  end
+
+  describe "#insert" do
+    context "when a column is empty" do
+      it "adds an item to the lowest row on the column" do
+        item = "X"
+        col = 4
+        expect { board_init.insert(item, col) }.to change { board_init.instance_variable_get(:@grid)[5][col] }.from(nil).to(item)
+      end
+    end
+
+    context "when a column is not empty" do
+      let(:grid_non_empty) do
+        [[nil, nil, nil, nil, nil, nil, nil],
+         [nil, nil, nil, nil, nil, nil, nil],
+         [nil, nil, nil, nil, nil, nil, nil],
+         [nil, nil, nil, nil, nil, nil, nil],
+         [nil, nil, nil, nil, "B", nil, nil],
+         [nil, nil, nil, nil, "A", nil, nil]]
+      end
+      subject(:board_not_empty) { described_class.new(grid_non_empty) }
+
+      it "adds item to the lowest empty row in the columm" do
+        item = "C"
+        col = 4
+        expect { board_not_empty.insert(item, col) }.to change { board_not_empty.instance_variable_get(:@grid)[3][col] }.from(nil).to(item)
       end
     end
   end
