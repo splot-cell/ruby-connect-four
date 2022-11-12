9347d6a5c21bdf6e0cbb24f649bf842aa0af3f27
Olly Seber <olly_s@hotmail.co.uk>
2022-11-12 15:29:32 -0800

    Write test for validating user input

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -3,5 +3,22 @@
 require_relative "../lib/connect_four"
 
 describe ConnectFour do
+  let(:board) { double("board", num_cols: 7) }
+  subject(:game_init) { described_class.new(board) }
+
+  describe "#validate_input" do
+    context "when the board has 7 columns" do
+      context "when the board is empty" do
+        before do
+          allow(board).to receive(:column_full?).and_return(false)
+        end
+        context "when the input is 3" do
+          it "returns true" do
+            expect(game_init.validate_input(3)).to be true
+          end
+        end
+      end
+    end
+  end
 
 end
